0e72880fce6b370d8d18cd94844c82ac1a298c1d
Daniel Newman <dwnewman78@gmail.com>
2018-09-04 15:26:17 -0500

    I've done a lot since the last commit

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+apple = input("What is your favorite fruit? ")
+orange = input("What is your favorite color? ")
+
+print(orange + " " + apple)
